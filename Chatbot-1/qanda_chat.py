e9b567859b822bf4f80d0a264b489a9dd506e0a5
Ashmita Gurung <94732442+Ashmitagurung@users.noreply.github.com>
2025-02-01 21:34:10 +0545

    Add files via upload

---
+++ Chatbot-1/qanda_chat.py
@@ -0,0 +1,46 @@
+
+from dotenv import load_dotenv
+load_dotenv()
+
+import streamlit as st
+import os
+import google.generativeai as genai
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+## Function to load Gemini pro model and get response
+
+model=genai.GenerativeModel("gemini-pro")
+chat=model.start_chat(historu=[])
+
+def get_gemini_response(question):
+    response=chat.send_message(question,stream=True)
+    return response
+
+##Initialize our Streamlit app
+
+st.set_page_config(page_title="qanda Demo")
+
+st.header("Gemini LLM Application")
+
+##Initialize the session state for chat history if it doesnot exist
+
+if 'chat_history' not in st.session_state:
+    st.session_state['chat_history'] = []
+
+inpu=st.text_inputt("Input: ",key="input")
+submit=st.button("Ask the questions")
+
+if submit and input:
+    response=get_gemini_response(input)
+
+    ##Add user query and response to session chat history
+    st.session_state['chat_history'.append(("You",input))]
+    st.subheader("The response is ")
+    for chunk in response:
+        st.write(chunk.text)
+        st.session_state['chat_history'.append(("Bot",chunk.text))]
+st.subheader("the history is ")
+
+for role,text in st.session_state['chat_history']:
+    st.write(f"{role}:{text}")
